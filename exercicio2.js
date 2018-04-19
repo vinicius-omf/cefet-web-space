04dea20c6a5160f25a57e0d5c46f7259df977faf
Vinicius <vinicius-omf@hotmail.com>
2018-04-19 09:01:51 -0300

    exercicios 1 e 2

--- exercicio2.js
+++ exercicio2.js
@@ -9,3 +9,17 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+
+var index = 0;
+
+function proximaImagem(){
+	index = index >=4 ? 0 : index+1;
+	let img = document.getElementById('slide');
+	img.src = servidorDasImagens + todasAsImagens[index];
+}
+
+function anteriorImagem(){
+	index = index <=0 ? 4 : index-1;
+	let img = document.getElementById('slide');
+	img.src = servidorDasImagens + todasAsImagens[index];
+}
